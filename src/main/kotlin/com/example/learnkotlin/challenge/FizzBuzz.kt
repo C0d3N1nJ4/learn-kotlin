a1ad1cdb15b7c5e6a9e9d0a4d5566fce5cc4cb33
naiomi <Naiomi.Naidoo@icloud.com>
2023-08-01 10:46:51 +0200

    Alternative challenge solution - fizzbuzz

--- src/main/kotlin/com/example/learnkotlin/challenge/FizzBuzz.kt
+++ src/main/kotlin/com/example/learnkotlin/challenge/FizzBuzz.kt
@@ -2,7 +2,9 @@ package com.example.learnkotlin.challenge
 
 fun main(args: Array<String>) {
     println("Fizz Buzz Challenge")
-    fizzBuzz()
+    fizzBuzzSolution1()
+    println()
+    fizzBussSolution2()
 
 }
 
@@ -11,7 +13,7 @@ fun main(args: Array<String>) {
 //print Buzz if the number is divisible by 5
 //print Fizz Buzz if the number is divisible by both 3 and 5
 //print the number if it is not divisible by 3 or 5
-fun fizzBuzz() {
+fun fizzBuzzSolution1() {
     for (i in 1..105) {
         if ((i % 3 == 0) && (i % 5 == 0)) {
             println("$i : Fizz Buzz")
@@ -27,5 +29,15 @@ fun fizzBuzz() {
     }
 }
 
+fun fizzBussSolution2() {
+    for (i in 1..105) {
+        when {
+            (i % 3 == 0) && (i % 5 == 0) -> println("$i : Fizz Buzz")
+            i % 3 == 0 -> println("$i : Fizz")
+            i % 5 == 0 -> println("$i : Buzz")
+            else -> println(i)
+        }
+    }
+}
 
 
