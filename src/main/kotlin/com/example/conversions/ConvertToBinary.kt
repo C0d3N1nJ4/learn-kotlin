043ccb0aa8a0157353f87738fb867912482e9c1a
naiomi <Naiomi.Naidoo@icloud.com>
2023-08-15 21:34:48 +0200

    Convert an integer to binary

--- src/main/kotlin/com/example/conversions/ConvertToBinary.kt
+++ src/main/kotlin/com/example/conversions/ConvertToBinary.kt
@@ -1,8 +1,17 @@
 package com.example.conversions
 
+//print a number in a base other than base 10
 fun main(args: Array<String>) {
 val number = 42
     println("The number is $number")
     println("The number in binary is ${number.toString(2)}")
+
+    //binary joke
+    val joke = """
+        Actually there are ${3.toString(3)} types of people,
+        Those who understand binary and those who don't.
+        And those who didn't realise this is a ternary joke.
+    """.trimIndent()
+    println(joke)
 }
 
