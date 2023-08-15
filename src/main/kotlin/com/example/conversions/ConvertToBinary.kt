7739598e10b3933f31482120fe3c757ae1db9229
naiomi <Naiomi.Naidoo@icloud.com>
2023-08-15 21:26:37 +0200

    Convert an integer to binary

---
+++ src/main/kotlin/com/example/conversions/ConvertToBinary.kt
@@ -0,0 +1,8 @@
+package com.example.conversions
+
+fun main(args: Array<String>) {
+val number = 42
+    println("The number is $number")
+    println("The number in binary is ${number.toString(2)}")
+}
+
