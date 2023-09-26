f22e613a4c05ee4c47294b7f1a0e064ba54e6448
naiomi <Naiomi.Naidoo@icloud.com>
2023-09-26 15:11:29 +0200

    Using method chaining in Kotlin

--- src/main/kotlin/com/example/learnkotlin/MethodChaining.kt
+++ src/main/kotlin/com/example/learnkotlin/MethodChaining.kt
@@ -1,4 +1,21 @@
 package com.example.learnkotlin
 
-class MethodChaining {
+fun sequences() {
+    val students = getStudents()
+    val sqStudents = students.drop(1).take(3).toList()
+    println("sqStudents : " + sqStudents)
+
+    val numbers = generateSequence(100) { it + 1 }
+    println("Numbers:" + numbers.drop(5).take(20).toList())
+
+    val squares = generateSequence(1) { it + 1 }.map { it * it }
+    val evenSquares = squares.filter { it % 2 == 0 }
+    println("Even Squares:" + evenSquares.take(5).toList())
+
+    val fib = generateSequence(1 to 1) { it.second to it.first + it.second }.map { it.first }
+    println("Fibonacci:" + fib.take(10).toList())
+}
+
+fun main(args: Array<String>) {
+    sequences()
 }
